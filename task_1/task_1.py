33e110e64a7c0d7b3f8aeb6058ee14a6363a235a
Volodymyr Litvinchuk <vova@jajafinance.com>
2024-07-16 09:57:01 +0300

    Fixed spacing

--- task_1/task_1.py
+++ task_1/task_1.py
@@ -18,7 +18,6 @@ def total_salary(path):
     except Exception as e:
         print(f"Error: {e}")
         return (0, 0)
-    
-    
+      
 total, average = total_salary("task_1/salary_file.txt")
 print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
